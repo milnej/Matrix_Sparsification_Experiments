4c48d2ced1d7290d0d4d6bf6da717a7738bb1898
James Milne <jamesmilne925@gmail.com>
2019-11-27 12:53:26 -0500

    grid

--- Experiments.py
+++ Experiments.py
@@ -6,35 +6,37 @@ import sys
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-def run_exp(A, test_points, trials, title):
+def run_exp(A, trials, title):
 
     n = len(A)
 
     # plot s vs error
-    error_scale = [i/10 for i in range(3,10)]
+    error_scale = [i/10 for i in range(3,10, 2)]
     s_scale = [s_calc(A,error_scale[i], n) for i in range(len(error_scale))]
+    samples = np.linspace(s_scale[-1], s_scale[0], len(error_scale))
 
     print(stable_rank(A))
 
     # each index - ((error, samples), success_rate)
-    success_array = []
+    success_matrix = []
+    p = 0
 
-    for p in range(test_points):
-        error_p = np.random.uniform(error_scale[0], error_scale[-1])
+    for i in range(len(error_scale)):
+        error_p = error_scale[i]
+        for j in range(len(samples)):
+            sample_p = int(samples[j])
+
+            success_matrix.append([])
+            num_success = 0
+            print('New trial set:', p)
+            for t in range(trials):
+                print('trial:', t)
+                A_til = sparsify(A, error_p, n, sample_p, True)
+                num_success += sparse_error_success(A, A_til, error_p)
+            success_matrix[i].append(((error_p, sample_p), num_success/trials))
+            
+            p+=1
         
-        # samples a high amount of values s (based on the paper)
-        sample_p = int(np.random.uniform(s_scale[-1], s_scale[0]))
-
-        # samples a low amount of values s (2n - n^2)
-        # sample_p = int(np.random.uniform(2*n, n**2))
-        num_success = 0
-        print('New trial set:', p)
-        for t in range(trials):
-            print('trial:', t)
-            A_til = sparsify(A, error_p, n, sample_p, True)
-            num_success += sparse_error_success(A, A_til, error_p)
-        success_array.append(((error_p, sample_p), num_success/trials))
-    
     
     plt.plot(error_scale,s_scale)
     plt.xlabel('Error percent')
@@ -42,11 +44,13 @@ def run_exp(A, test_points, trials, title):
     plt.title(title)
 
     success_threshold = .5
-    for val in range(test_points):
-        if success_array[val][1] >= success_threshold:
-            plt.plot(success_array[val][0][0], success_array[val][0][1], 'go')
-        else:
-            plt.plot(success_array[val][0][0], success_array[val][0][1], 'ro')
+    for i in range(len(error_scale)):
+        for j in range(len(samples)):
+
+            if success_matrix[i][j][1] >= success_threshold:
+                plt.plot(success_matrix[i][j][0][0], success_matrix[i][j][0][1], 'go')
+            else:
+                plt.plot(success_matrix[i][j][0][0], success_matrix[i][j][0][1], 'ro')
 
     
     plt.show()
@@ -63,9 +67,8 @@ if __name__ == '__main__':
     # plt.imshow(beach_img, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
     # plt.show()
 
-    test_points = 20
     trials = 1
 
-    run_exp(img_gray, test_points, trials, 'Beach Image')
+    run_exp(img_gray, trials, 'Beach Image')
     
 
